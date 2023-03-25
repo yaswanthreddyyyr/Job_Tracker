ac299db183d58c9e80a41dfb46c63f76c79ab22b
yaswanth reddy <reddyyaswanth57@gmail.com>
2023-03-25 15:42:00 +0530

    kuhuc

---
+++ client/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+const Loading = ({ center }) => {
+
+  
+
+  return <div className='center'><ReactLoading type={'cylon'} color={'#2cb1bc'} height={'10%'} width={'10%'} /></div>
+}
+
+export default Loading
